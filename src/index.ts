b1999d90332b6ead2235cc52af6d19c4a1c5b89e
Craig McCartney <craig.a.mccartney@gmail.com>
2019-03-14 18:25:29 +0000

    End of third day

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,34 @@
 import './styles.css';
-import { add, PI } from './utils';
+import { getRandomInt } from './utils';
 
-console.log('Ready to Party');
+const squares = document.querySelectorAll('.square');
 
-console.log(add(10, 18));
+const secret = getRandomInt(0, 5);
+let guesses = 0;
 
-console.log(PI);
+const winnerMessage = document.getElementById('guessMessage') as HTMLSpanElement;
+
+squares.forEach((sq, idx) => {
+    const el = sq as HTMLDivElement;
+    if ((idx) === secret) {
+        el.dataset.secret = 'true';
+    }
+    sq.addEventListener('click', handleClick)
+
+});
+
+function handleClick() {
+    guesses = guesses + 1;
+    const el = this as HTMLDivElement;
+    if (el.dataset.secret) {
+        el.classList.add('winner');
+        squares.forEach(sq => {
+            sq.removeEventListener('click', handleClick)
+        })
+        winnerMessage.innerHTML = `<small> - You guessed this in <b>${guesses}</b> guesses!!!</small>`;
+    } else {
+        el.classList.add('loser');
+    }
+    el.removeEventListener('click', handleClick);
+
+}
